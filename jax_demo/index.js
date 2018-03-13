674dda4d807e5c260a4ca23fa68a95824e7d57fe
jbjw <github@jbjw.com>
2018-03-13 14:47:01 -0700

    eine commit

--- jax_demo/index.js
+++ jax_demo/index.js
@@ -11,6 +11,21 @@ navigator.geolocation.getCurrentPosition( geoSuccess, geoError, geoOptions )
 
 // qs(  )
 
+function haversineDistance( pos1, pos2 ) {
+	var earthRadius = 6371e3 // meters
+	var lat1 = pos1.coords.latitude.toRadians()
+	var lat2 = pos2.coords.latitude.toRadians()
+	var dLat = ( pos2.coords.latitude - pos1.coords.latitude ).toRadians()
+	var dLon = ( pos2.coords.longitude - pos1.coords.longitude ).toRadians()
+	var a =
+		Math.sin( dLat / 2 ) * Math.sin( dLat / 2 ) +
+		Math.cos( lat1 ) * Math.cos( lat2 ) *
+		Math.sin( dLon / 2 ) * Math.sin( dLon / 2 )
+	var c = 2 * Math.atan2( Math.sqrt( a ), Math.sqrt( 1-a ) )
+	var distance = earthRadius * c
+	return distance
+}
+
 var geoOptions = {
 	enableHighAccuracy: true,
 	maximumAge: 30000,
