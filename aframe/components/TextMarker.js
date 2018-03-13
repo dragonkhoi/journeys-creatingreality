9611e939ebd1b4e2f01d20868154459b926b048e
Khoi Le <khoile@stanford.edu>
2018-03-13 13:43:46 -0700

    test planes

--- aframe/components/TextMarker.js
+++ aframe/components/TextMarker.js
@@ -1,13 +1,18 @@
 AFRAME.registerComponent('text-marker', {
   schema: {
     message: { type: 'string', default: 'I like this spot!' },
-    width: { type: 'number', default: 1 },
-    height: { type: 'number', default: 1 },
-    color: { type: 'color', default: "#000" }
+    width: { type: 'number', default: auto },
+    height: { type: 'number', default: auto },
+    textColor: { type: 'color', default: "#FFF" },
+    planeColor: { type: 'color', default: "#333"},
+    position: { type: 'string', default: "0 0 0"}
   },
   init: function(){
     console.log(this.data.message);
-    this.el.setAttribute("text", "value: " + this.data.message);
+    this.el.setAttribute("text", "value: " + this.data.message + "; color: " + this.data.textColor);
+    this.el.setAttribute("geometry", "primitive: plane; width: " + this.data.width + "; height: " + this.data.height);
+    this.el.setAttribute("material", "color: " + this.data.planeColor);
+    this.el.setAttribute("position", this.data.position);
     // set the
   }
 });
