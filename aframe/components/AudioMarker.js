43537de33ed67e89c3fe618ca29a7ae45098ae93
Prarthana S <prarthana-s@users.noreply.github.com>
2018-03-13 16:00:18 -0700

    Edit autoplay

--- aframe/components/AudioMarker.js
+++ aframe/components/AudioMarker.js
@@ -12,14 +12,15 @@ AFRAME.registerComponent('audio-marker', {
         console.log("AUDIO");
       console.log(this.data);
       console.log(this.data.audioUri);
-      this.el.setAttribute('sound', 'src:url(' + this.data.audioUri + ')');
+      this.el.setAttribute('sound', 'src:url(' + this.data.audioUri + '); autoplay:true');
     //   this.el.setAttribute("src", this.data.audioUri);
-      this.el.setAttribute("autoplay", true);      
+    //   this.el.setAttribute("autoplay", true);      
     //   this.el.setAttribute("text", "value: " + this.data.message + "; color: " + this.data.textColor + "; align: center");
     //   this.el.setAttribute("geometry", "primitive: plane; width: auto; height: auto");
     //   this.el.setAttribute("material", "color: " + this.data.planeColor);
       this.el.setAttribute("position", this.data.position);
       // set the
+      console.log(this);
     }
   });
   
