931ee79fe00a145a41bf54325317c992b4b8a6b5
Prarthana S <prarthana-s@users.noreply.github.com>
2018-03-13 15:49:31 -0700

    Fix audio errors

--- aframe/components/AudioMarker.js
+++ aframe/components/AudioMarker.js
@@ -2,15 +2,17 @@ AFRAME.registerComponent('audio-marker', {
     schema: {
       audioUri: { type: 'string', default: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3' },
       audioDescription: { type: 'string', default: 'I love this song!' },
-    //   audioIcon: { type: 'string', default: 'http://www.eenewsanalog.com/sites/default/files/styles/inner_article/public/sites/default/files/images/audioicon700.jpg?itok=vePxeMyn' },
-    //   width: { type: 'number', default: 1 },
-    //   height: { type: 'number', default: 1 },
-    //   planeColor: { type: 'color', default: "#333"},
+      audioIcon: { type: 'string', default: 'http://www.eenewsanalog.com/sites/default/files/styles/inner_article/public/sites/default/files/images/audioicon700.jpg?itok=vePxeMyn' },
+      width: { type: 'number', default: 1 },
+      height: { type: 'number', default: 1 },
+      planeColor: { type: 'color', default: "#333"},
       position: { type: 'string', default: "0 0.005 -0.5"}
     },
     init: function(){
+        console.log("AUDIO");
       console.log(this.data);
-      this.el.setAttribute('sound', 'src:url("' + this.data.audioUri + '")');
+      console.log(this.data.audioUri);
+      this.el.setAttribute('sound', 'src:url(' + this.data.audioUri + ')');
     //   this.el.setAttribute("src", this.data.audioUri);
       this.el.setAttribute("autoplay", true);      
     //   this.el.setAttribute("text", "value: " + this.data.message + "; color: " + this.data.textColor + "; align: center");
