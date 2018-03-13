4ba4ed20761a76797c62b0f425355797901b2d4b
Khoi Le <khoile@stanford.edu>
2018-03-13 14:22:34 -0700

    create waypoints function

--- 3js/JourneySession.js
+++ 3js/JourneySession.js
@@ -1,16 +1,34 @@
 function JourneySession(journeyObject) {
   this.journeyObject = journeyObject;
   this.waypoints = journeyObject.Waypoints;
+  this.wpObjs = [];
   this.nextWaypoint = 0; // journeyObject.waypoints[0]
   this.goToNextWaypoint = goToNextWaypoint;
 }
 
-// function to create ContentPoint from given JSON content
+// model: function to create ContentPoint from given JSON content
 
-// function to create AR Component from ContentPoint
+// view: function to create AR Component from ContentPoint
+
+// model: function to create Waypoint from given JSON waypoint
 
 function goToNextWaypoint(){
   this.nextWaypoint++;
+  // update view by deactivating current waypoint and activating next
+}
+
+function createWaypoints(){
+  for(var i = 0; i < this.waypoints.length; i++){
+    var curWP = this.waypoints[i];
+    var newContentPoints = [];
+    for(var j = 0; j < curWP.contentPoints.length; j++){
+      var curCP = curWP.contentPoints[j];
+      var newCP = new ContentPoint(curCP.type, curCP.reference, curCP.orientation);
+      newContentPoints.push(newCP);
+    }
+    var newWP = new Waypoint(curWP.lat, curWP.lng, newContentPoints);
+    this.wpObjs.push(newWP);
+  }
 }
 
 function displayMarkers(){
---
+++ 3js/Waypoint.js
@@ -0,0 +1,5 @@
+function Waypoint(lat, lng, contentPoints){
+  this.lat = lat;
+  this.lng = lng;
+  this.contentPoints = contentPoints;
+}
