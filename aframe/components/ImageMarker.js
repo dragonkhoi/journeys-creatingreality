a1d20438f51762d1924b59410068083dfd302351
Prarthana S <prarthana-s@users.noreply.github.com>
2018-03-13 20:06:28 -0700

    ImageMarker Component

--- aframe/components/ImageMarker.js
+++ aframe/components/ImageMarker.js
@@ -2,13 +2,13 @@ AFRAME.registerComponent('img-marker', {
   schema: {
     imgUri: { type: 'string', default: 'https://www.merriam-webster.com/assets/mw/images/article/art-wap-article-main/puppy-3143-7cfb4d6a42dfc7d9d1ae7e23126279e8@1x.jpg' },
     imgDescription: { type: 'string', default: 'I like this spot!' },
-    width: { type: 'number', default: 1 },
-    height: { type: 'number', default: 1 },
+    width: { type: 'number', default: 100 },
+    height: { type: 'number', default: 100 },
     position: { type: 'string', default: "0 0 0"}
   },
   init: function(){
-    this.el.setAttribute("src", this.data.imgUri);
-    this.el.setAttribute("height", this.data.height);
-    this.el.setAttribute("width", this.data.width);
+    this.el.setAttribute("position", this.data.position);
+    this.el.setAttribute("geometry", "width: 100; height: 100");
+    this.el.setAttribute("material", "src:" + this.data.imgUri);
   }
 });
