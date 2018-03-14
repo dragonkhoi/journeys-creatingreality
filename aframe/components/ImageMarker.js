8675452e6ead407693c585e561c6cfb34c140121
Prarthana S <prarthana-s@users.noreply.github.com>
2018-03-14 01:53:30 -0700

    Calculation of positions

--- aframe/components/ImageMarker.js
+++ aframe/components/ImageMarker.js
@@ -2,13 +2,13 @@ AFRAME.registerComponent('img-marker', {
   schema: {
     imgUri: { type: 'string', default: 'https://www.merriam-webster.com/assets/mw/images/article/art-wap-article-main/puppy-3143-7cfb4d6a42dfc7d9d1ae7e23126279e8@1x.jpg' },
     imgDescription: { type: 'string', default: 'Image' },
-    width: { type: 'number', default: 100 },
-    height: { type: 'number', default: 100 },
+    width: { type: 'number', default: 2.5 },
+    height: { type: 'number', default: 2.5 },
     position: { type: 'string', default: "0 0 0"}
   },
   init: function(){
     this.el.setAttribute("position", this.data.position);
-    this.el.setAttribute("geometry", "width: 100; height: 100");
+    this.el.setAttribute("geometry", "width: 2.5; height: 2.5");
     this.el.setAttribute("material", "src:" + this.data.imgUri);
   }
 });
