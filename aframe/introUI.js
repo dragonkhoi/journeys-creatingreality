49cd24197d2c69a3f0fbbd371bccaa0b4d317914
Khoi Le <khoile@stanford.edu>
2018-03-14 01:54:13 -0700

    share bar to take photo works

--- aframe/introUI.js
+++ aframe/introUI.js
@@ -3,10 +3,61 @@ window.onload = function() {
   showStory.addEventListener("touchstart", function(event){
     showStoryHover();
   }, false);
-  var showStoryPlus = document.getElementById("showStoryPlus");
+   var showStoryPlus = document.getElementById("showStoryPlus");
   showStoryPlus.addEventListener("touchend", function(event){
     hideStoryHover();
   }, false);
+
+   var plusButton = document.querySelector("#expandShareMenu");
+  plusButton.addEventListener("touchstart", function(event){
+    showPlusHit();
+  }, false);
+  var plusButtonHit = document.querySelector("#expandShareMenuHit");
+  plusButtonHit.addEventListener("touchend", function(event){
+    activateShareMenu();
+  }, false);
+  plusButtonHit.addEventListener("touchstart", function(event){
+    closeShareMenu();
+  }, false);
+}
+
+function showPlusHit(){
+  var plusButton = document.querySelector("#expandShareMenu");
+  plusButton.style.display = "none";
+  var plusButtonHit = document.querySelector("#expandShareMenuHit");
+  plusButtonHit.style.display = "block";
+}
+
+function activateShareMenu(){
+  var plusButtonHit = document.querySelector("#expandShareMenuHit");
+  plusButtonHit.style.display = "block";
+  var shareButtonBar = document.querySelector("#shareButtonBar");
+  shareButtonBar.style.display = "block";
+}
+
+function closeShareMenu(){
+  var plusButtonHit = document.querySelector("#expandShareMenuHit");
+  if(plusButtonHit.style.display == "block"){
+    plusButtonHit.style.display = "none";
+    var shareButtonBar = document.querySelector("#shareButtonBar");
+    shareButtonBar.style.display = "none";
+  }
+}
+
+function activateCamera(){
+  var photoActivate = document.querySelector("#takePhotoText");
+  var photoActivateBlack = document.querySelector("#takePhotoTextBlack");
+  var takePhotoOverlay = document.querySelector("#takePhotoOverlay");
+  if(takePhotoOverlay.style.display == "block"){
+    takePhotoOverlay.style.display = "none";
+    photoActivateBlack.style.display = "none";
+    photoActivate.style.display = "block";
+  }else {
+    takePhotoOverlay.style.display = "block";
+    photoActivateBlack.style.display = "block";
+    photoActivate.style.display = "none";
+
+  }
 }
 
 function startButton(){
@@ -16,7 +67,8 @@ function startButton(){
 }
 
 function showStoryHover(){
-  document.getElementById("showStoryPlus").style.display = "block";
+  showStoryPlus = document.getElementById("showStoryPlus");
+  showStoryPlus.style.display = "block";
 }
 
 function hideStoryHover(){
@@ -29,6 +81,6 @@ function hideStoryHover(){
 
 function startJourney(){
   document.getElementById("introScreen").style.display = "none";
-  document.getElementById("takePhotoOverlay").style.display = "block";
+  document.getElementById("plusOverlay").style.display = "block";
 
 }
