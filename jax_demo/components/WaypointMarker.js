ec17e4f6ac7b8dac1e8782fa7840dfc6156a7f7a
jbjw <github@jbjw.com>
2018-03-13 22:10:26 -0700

    auto-save

--- jax_demo/components/WaypointMarker.js
+++ jax_demo/components/WaypointMarker.js
@@ -5,6 +5,7 @@ AFRAME.registerComponent( "waypoint-marker", {
 		width: { type: "number", default: 1 },
 		height: { type: "number", default: 1 },
 		depth: { type: "number", default: 1 },
+		color: { type: "color", default: "#FFF" },
 		textColor: { type: "color", default: "#FFF" },
 		planeColor: { type: "color", default: "#333" },
 		position: { type: "string", default: "0 0 0" },
@@ -12,6 +13,9 @@ AFRAME.registerComponent( "waypoint-marker", {
 		coords: { type: "vec2", default: "0 0" },
 	},
 	init: function () {
+		Math.radians = degrees => ( degrees * ( Math.PI * 2 ) ) / 360
+		Math.degrees = radians => ( radians * 360 ) / ( Math.PI * 2 )
+
 		this.getBearing = function getBearing( coords1, coords2 ) {
 			const lat1 = Math.radians( coords1.x ) // lat
 			const lon1 = Math.radians( coords1.y ) // lon
@@ -22,9 +26,7 @@ AFRAME.registerComponent( "waypoint-marker", {
 				Math.cos( lat1 ) * Math.sin( lat2 ) -
 				Math.sin( lat1 ) * Math.cos( lat2 ) * Math.cos( lon2 - lon1 )
 			const bearing = Math.degrees( Math.atan2( y, x ) )
-			bearing = ( bearing + 360 ) % 360
-
-			return bearing // degrees, -180 to 180, normalize with modulo
+			return ( bearing + 360 ) % 360 // degrees, -180 to 180, normalize with modulo
 		}
 
 		// Math.radians = function(degrees) {
@@ -34,10 +36,7 @@ AFRAME.registerComponent( "waypoint-marker", {
 		// // Converts from radians to degrees.
 		// Math.degrees = function(radians) {
 		//   return radians * 180 / Math.PI;
-		// };
-
-		Math.radians = degrees => ( degrees * ( Math.PI * 2 ) ) / 360
-		Math.degrees = radians => ( radians * 360 ) / ( Math.PI * 2 )
+		// }
 
 		// log( Math.degrees( Math.PI ) )
 		// log( Math.radians( 180 ) )
@@ -45,22 +44,23 @@ AFRAME.registerComponent( "waypoint-marker", {
 		this.getDistance = function getDistance( coords1, coords2 ) {
 			// haversine
 			var earthRadius = 6371e3 // meters
-			var lat1 = Math.radians( coords1.latitude )
-			var lat2 = Math.radians( coords2.latitude )
-			var dLat = Math.radians( ( coords2.latitude - coords1.latitude ) )
-			var dLon = Math.radians( ( coords2.longitude - coords1.longitude ) )
+			var lat1 = Math.radians( coords1.x ) // lat
+			var lat2 = Math.radians( coords2.x ) // lat
+			var dLat = Math.radians( ( coords2.x - coords1.x ) ) // lat - lat
+			var dLon = Math.radians( ( coords2.y - coords1.y ) ) // lon - lon
 			var a =
 				Math.sin( dLat / 2 ) * Math.sin( dLat / 2 ) +
 				Math.cos( lat1 ) * Math.cos( lat2 ) *
 				Math.sin( dLon / 2 ) * Math.sin( dLon / 2 )
 			var c = 2 * Math.atan2( Math.sqrt( a ), Math.sqrt( 1-a ) )
 			var distance = earthRadius * c
+			// console.log( "getDistance", lat1, lat2, dLat, dLon )
 			return distance // meters
 		}
 
-		this.el.setAttribute( "text", "value: " + this.data.message + "; color: " + this.data.textColor + "; align: center" )
+		// this.el.setAttribute( "text", "value: " + this.data.message + "; color: " + this.data.textColor + "; align: center" )
 		this.el.setAttribute( "geometry", "primitive: box; width: 1; height: 100; depth: 1;" )
-		this.el.setAttribute( "material", "color", this.data.planeColor )
+		this.el.setAttribute( "material", "color", this.data.color )
 		// this.el.setAttribute( "position", this.data.position )
 	},
 	tick: function () {
@@ -70,20 +70,26 @@ AFRAME.registerComponent( "waypoint-marker", {
 		// this.el.setAttribute( "waypoint-marker", "userCoords", { x: 1, y: 1, } )
 		// this.data.coords?
 		// this.el.set
+		// const userCoords = this.el.getAttribute( "userCoords" )
+		// const coords = this.el.getAttribute( "coords" )
+		// console.log( "in update", this.data.userCoords, this.data.coords )
+		const userCoords = this.data.userCoords
+		const coords = this.data.coords
+		if ( userCoords !== null ) {
 
-		console.log( this.el.getAttribute( "userCoords" ) )
-		// t
-		// const bearing = this.getBearing( , this.el.getAttribute( "coords" ) )
-		// const distance = this.getDistance( userPosition.coords, waypoints[ 0 ].coords )
-		//
-		// const x = distance * Math.sin( bearing )
-		// const y = 0.5
-		// const z = distance * Math.cos( bearing )
-		// this.el.object3D.position.x = x
-		// this.el.object3D.position.y = y
-		// this.el.object3D.position.z = z
-		// // position.set(1, 2, 3);
-		// // waypoint.setAttribute( "position", `${x} ${y} ${z}` )
+			const bearing = this.getBearing( userCoords, coords )
+			console.log( this.data.name, bearing )
+			const distance = this.getDistance( userCoords, coords )
+			const x = distance * Math.sin( Math.radians( bearing ) )
+			const y = 0.5
+			const z = distance * Math.cos( Math.radians( bearing ) )
+			this.el.object3D.position.x = x
+			this.el.object3D.position.y = y
+			this.el.object3D.position.z = -z
+			// // position.set(1, 2, 3);
+			// // waypoint.setAttribute( "position", `${x} ${y} ${z}` )
+		}
+
 	},
 
 } )
--- jax_demo/index.js
+++ jax_demo/index.js
@@ -7,35 +7,87 @@ const log = console.log
 const qs = document.querySelector.bind( document )
 const qsa = document.querySelectorAll.bind( document )
 
+// if ("geolocation" in navigator) {
+// 	alert("geo")
+// } else {
+// 	alert("nogeo")
+//   /* geolocation IS NOT available */
+// }
+
 // fetch
 const waypointsInfo = [
+	// {
+	// 	name: "ballroom",
+	// 	color: "black",
+	// 	coords: {
+	// 		latitude: 34.020097,
+	// 		longitude: -118.286134,
+	// 	},
+	// },
 	{
+		name: "testination",
+		color: "yellow",
 		coords: {
-			latitude: 34.021492,
-			longitude: -118.284852,
+			latitude: 34.020186,
+			longitude: -118.285841,
 		},
 	},
 	{
-		name: "fander's pork 🌳",
+		name: "north",
+		color: "red",
 		coords: {
-			latitude: 34.021492,
-			longitude: -118.284852,
+			latitude: 34.025437,
+			longitude: -118.286507,
 		},
 	},
 	{
-		name: "ol' starbsy",
+		name: "east",
+		color: "green",
 		coords: {
-			latitude: 34.018558,
-			longitude: -118.281852,
+			latitude: 34.019999,
+			longitude: -118.280922,
 		},
 	},
 	{
-		name: "cooliseum 😎",
+		name: "south",
+		color: "white",
 		coords: {
-			latitude: 34.014036,
-			longitude: -118.287854,
+			latitude: 34.014308,
+			longitude: -118.286079,
 		},
 	},
+	{
+		name: "west",
+		color: "blue",
+		coords: {
+			latitude: 34.020638,
+			longitude: -118.292826,
+		},
+	},
+	// {
+	// 	name: "fander's pork 🌳",
+	// 	color: "red",
+	// 	coords: {
+	// 		latitude: 34.021492,
+	// 		longitude: -118.284852,
+	// 	},
+	// },
+	// {
+	// 	name: "ol' starbsy",
+	// 	color: "brown",
+	// 	coords: {
+	// 		latitude: 34.018558,
+	// 		longitude: -118.281852,
+	// 	},
+	// },
+	// {
+	// 	name: "cooliseum 😎",
+	// 	color: "black",
+	// 	coords: {
+	// 		latitude: 34.014036,
+	// 		longitude: -118.287854,
+	// 	},
+	// },
 ]
 
 const scene = qs( "a-scene" )
@@ -47,13 +99,12 @@ for ( let waypointInfo of waypointsInfo ) {
 	const waypointEl = document.createElement( "a-entity" )
 	waypointEl.setAttribute( "waypoint-marker", {
 		"name": waypointInfo.name,
+		"color": waypointInfo.color,
 		"coords": { x: waypointInfo.coords.latitude, y: waypointInfo.coords.longitude, },
 	} )
 	waypointEls.push( waypointEl )
 	scene.appendChild( waypointEl )
 }
-
-let userPosition
 
 var geoOptions = {
 	enableHighAccuracy: true,
@@ -65,9 +116,10 @@ navigator.geolocation.getCurrentPosition( geoSuccess, geoError, geoOptions )
 var watchID = navigator.geolocation.watchPosition( geoSuccess, geoError, geoOptions )
 
 function geoSuccess( position ) {
+	// alert( "geoSuccess " + position.coords.latitude + " " + position.coords.longitude )
 	for ( let waypointEl of waypointEls ) {
-		console.log( waypointEl )
-		waypointEl.setAttribute( "user-coords", {
+		// console.log( waypointEl )
+		waypointEl.setAttribute( "waypoint-marker", "userCoords", {
 			x: position.coords.latitude,
 			y: position.coords.longitude,
 		} )
@@ -77,5 +129,26 @@ function geoSuccess( position ) {
 
 function geoError( error ) {
 	// error.code
-	alert( "Sorry, no position available." )
+	// alert( "Sorry, no position available. " + error.code )
 }
+
+
+
+function handleOrientation(event) {
+	var z = event.alpha // 0-360
+	var x = event.beta;  // In degree in the range [-180,180]
+	var y = event.gamma; // In degree in the range [-90,90]
+	console.log( x, y, z )
+
+	// Because we don't want to have the device upside down
+	// We constrain the x value to the range [-90,90]
+	if (x >  90) { x =  90};
+	if (x < -90) { x = -90};
+
+	// To make computation easier we shift the range of
+	// x and y to [0,180]
+	x += 90;
+	y += 90;
+	console.log( x, y, z )
+}
+window.addEventListener('deviceorientation', handleOrientation);
