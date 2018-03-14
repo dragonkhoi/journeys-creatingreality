c18454e525cebd5cd2362c175ea733f4f8e59731
Khoi Le <khoile@stanford.edu>
2018-03-14 13:49:14 -0700

    unity arrows

---
+++ UNITY/Journeys/Assets/WaypointObj.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointObj : MonoBehaviour {
+    public Waypoint waypoint;
+    public List<GameObject> contentPointGOs;
+
+    public void AddContentPointGO(GameObject go)
+    {
+        contentPointGOs.Add(go);
+    }
+	// Use this for initialization
+	void Start () {
+        contentPointGOs = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
